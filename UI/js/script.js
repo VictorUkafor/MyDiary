080c0b06b11ffdeeb59a82d052831600bf7fc574
Victor Ukafor <victorukafor@gmail.com>
2018-07-12 20:04:39 +0100

    Fixes bug in signin form, so that users can re-enter inputs and submit without refreshing the page

--- UI/js/script.js
+++ UI/js/script.js
@@ -27,43 +27,37 @@ function validateSignUpForm(){
 
     if(lastName.trim() === ''){
         const lastNameError = 'Last Name field is required';
-        document.getElementById('lastNameError').innerHTML += '<h1 class="errorField">'+ lastNameError +'</h1>';   
+        document.getElementById('lastNameError').innerHTML = '<h1 class="errorField">'+ lastNameError +'</h1>';   
         document.getElementById('LastName').value = "";   
     } 
     
     if(email.trim() === ''){
         const emailError = 'Email field is required';
-        document.getElementById('emailError').innerHTML += '<h1 class="errorField">'+ emailError +'</h1>'; 
+        document.getElementById('emailError').innerHTML = '<h1 class="errorField">'+ emailError +'</h1>'; 
         document.getElementById('Email').value = "";   
     }  
-    
-    if(email.trim() !== '' && !email.trim().indexOf('@') > -1){
-        const emailError = 'You\'ve entered an invalid email' ;
-        document.getElementById('emailError').innerHTML += '<h1 class="errorField">'+ emailError +'</h1>'; 
-        document.getElementById('Email').value = "";   
-    } 
 
     if(password.trim() === ''){
         const passwordError = 'Password field is required';
-        document.getElementById('passwordError').innerHTML += '<h1 class="errorField">'+ passwordError +'</h1>'; 
+        document.getElementById('passwordError').innerHTML = '<h1 class="errorField">'+ passwordError +'</h1>'; 
         document.getElementById('Password1').value = "";    
     } 
     
     if(confirmPassword.trim() === ''){
         const confirmPasswordError = 'Confirm Password field is required';
-        document.getElementById('confirmPasswordError').innerHTML += '<h1 class="errorField">'+ confirmPasswordError +'</h1>';
+        document.getElementById('confirmPasswordError').innerHTML = '<h1 class="errorField">'+ confirmPasswordError +'</h1>';
         document.getElementById('Password2').value = "";  
     } 
 
     if(confirmPassword.trim() !== '' && password.trim() !== confirmPassword.trim()){
-        const confirmPasswordError = 'Passwords do not match';
-        document.getElementById('confirmPasswordError').innerHTML += '<h1 class="errorField">'+ confirmPasswordError +'</h1>';  
+        const confirmPasswordError1 = 'Passwords do not match';
+        document.getElementById('confirmPasswordError').innerHTML = '<h1 class="errorField">'+ confirmPasswordError1 +'</h1>';  
         document.getElementById('Password2').value = "";    
     } 
 
     if(photograph.trim() === ''){ 
         const photographError = 'You must upload your passport';
-        document.getElementById('photographError').innerHTML += '<h1 class="errorField">'+ photographError +'</h1>';
+        document.getElementById('photographError').innerHTML = '<h1 class="errorField">'+ photographError +'</h1>';
         document.getElementById('Photograph').value = "";    
     }   
 
@@ -82,18 +76,32 @@ function validateSignInForm(){
     let email = document.forms["signIn"]["email"].value;
     let password = document.forms["signIn"]["password"].value;
 
+    document.getElementById('emailError').innerHTML = '';
+    document.getElementById('passwordError').innerHTML = '';
+
+    if(email.trim() !== '' && password.trim() !== ''){
+        return true;
+    } else {
+
     if(email.trim() === ''){
         const emailError = 'Email field is required';
         document.getElementById('emailError').innerHTML = '<h1 class="errorField">'+ emailError +'</h1>'; 
-    }  
+        document.getElementById('Email').value = "";
+    } 
     
     if(password.trim() === ''){
         const passwordError = 'Password field is required';
         document.getElementById('passwordError').innerHTML = '<h1 class="errorField">'+ passwordError +'</h1>'; 
-    }   
+        document.getElementById('Password1').value = "";
+    }  
 
     return false;
 }
+
+    return false;
+}
+
+
 
 
 function validateAddEntryForm(){
