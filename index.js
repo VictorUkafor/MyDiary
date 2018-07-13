b48610b00d1ab731d3879d6015ea1cc1c740e19b
Victor Ukafor <victorukafor@gmail.com>
2018-07-14 00:31:43 +0100

    Adds routes file and adds url version "api/v1"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import apiRouter from './server/routes/index';
 
 const app = express();
 const port = 8000;
@@ -8,8 +9,7 @@ const port = 8000;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => res.status(200).send({ message: 'Welcome to the MyDiary app!' }));
+app.use('/api/v1', apiRouter);
 
 if(!module.parent){ app.listen(port); }
 
