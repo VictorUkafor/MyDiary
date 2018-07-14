72dfd6a63e4e5578b15e8617368a2885c518caae
Victor Ukafor <victorukafor@gmail.com>
2018-07-14 12:07:36 +0100

    Adds code comments to controllers/user.js and middlewares/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,24 +1,10 @@
 import express from 'express';
-import uuid from 'uuid';
-import data from '../dummy_data';
-import UserController from '../controllers/users';
-import AuthController from '../middlewares';
 
 const apiRouter = express.Router();
-const auth = new AuthController(data);
-const user = new UserController(data, uuid);
 
 apiRouter.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to MyDiary app!',
 }));
-
-
-apiRouter.post(
-  '/signup',
-  auth.checksForUserRequiredFields,
-  auth.checksIfUserAlreadyExist,
-  user.postUser
-);
 
 
 export default apiRouter;
