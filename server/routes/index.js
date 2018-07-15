04d8a1b58c1924b824f8e785d344a57620d39849
Victor Ukafor <victorukafor@gmail.com>
2018-07-15 02:47:55 +0100

    Adds logic for the functionality of "GET /api/v1/entries" endpoint

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import uuid from 'uuid';
 import UserController from '../controllers/users';
+import EntryController from '../controllers/entries';
 import AuthController from '../middlewares';
 
 const apiRouter = express.Router();
 const auth = new AuthController();
 const user = new UserController(uuid);
+const entry = new EntryController(uuid);
 
 apiRouter.get('/', (req, res) => res.status(200).send({
   message: 'Welcome to MyDiary app!',
@@ -24,6 +26,11 @@ apiRouter.post(
   auth.checksForLogInRequiredFields,
   auth.checksIfUserExist,
   user.loginUser
+);
+
+apiRouter.get(
+  '/entries',
+  entry.getAllEntries
 );
 
 
