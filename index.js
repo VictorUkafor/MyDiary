df0db71463b02f8a7edc8852a338027d0e8bb5bd
Victor Ukafor <victorukafor@gmail.com>
2018-07-15 19:04:17 +0100

    Adds logic for unit testing for "GET /api/v1/entries/<entryId>" API endpoint

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 8000;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1', apiRouter);
 
 if(!module.parent){ app.listen(port); }
--- test/entries.js
+++ test/entries.js
@@ -1,9 +1,10 @@
 import supertest from 'supertest';
-import app from '../index';
 import { expect } from 'chai';
+import { entries } from '../server/dummy_data';
+import app from '../index';
+
 
 const request = supertest(app);
-
 
 describe('MyDiary API Routes', () => {
     beforeEach((done) => {
@@ -32,6 +33,26 @@ describe('MyDiary API Routes', () => {
     });
   });
 
+    // Testing for GET /api/v1/entries/<entryId>
+    describe('GET /api/v1/entries/<entryId>', () => {
+     // Entry can not be found
+      it('Entry can not be found', (done) => {
+        request.get('/api/v1/entries/1')
+          .expect(404)
+          .end((err) => {
+            done(err);
+          });
+      });
+  
+      // Gets a single entry
+      it('Gets a single entry', (done) => {
+        request.get(`/api/v1/entries/${entries[0].id}`)
+          .expect(200)
+          .end((err) => {
+            done(err);
+          });
+      });
+    });
 
 
     
