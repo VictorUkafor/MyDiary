62731e6c97f78e638efde09e2dce4c274ec8d7fd
Victor Ukafor <victorukafor@gmail.com>
2018-07-25 15:17:16 +0100

    Setup Database with raw sql on Postgres

---
+++ server/models/index.js
@@ -0,0 +1,22 @@
+const pg = require('pg');
+const connectionString = 'postgres://postgres:success4me@localhost:5432/mydiary_dev';
+
+const client = new pg.Client(connectionString);
+client.connect();
+
+const user = client.query(`CREATE TABLE IF NOT EXISTS diaryUser(id SERIAL PRIMARY KEY,
+  firstName VARCHAR(20) not null,
+  lastName VARCHAR(20) not null, 
+  email VARCHAR(40) not null, 
+  password VARCHAR(255) not null)`);
+
+user.on('end', () => { client.end(); });
+
+
+const entry = client.query(`CREATE TABLE IF NOT EXISTS entry(id SERIAL PRIMARY KEY, 
+  diaryUserId integer not null, 
+  title VARCHAR(50) not null, 
+  content text not null, 
+  foreign key (diaryUserId) REFERENCES diaryUser (id))`);
+
+entry.on('end', () => { client.end(); });
