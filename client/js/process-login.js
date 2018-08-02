948596732e6687ee18edd30e2dcff73afd63bf8a
Victor Ukafor <victorukafor@gmail.com>
2018-08-02 16:27:18 +0100

    Adds logic for processing login form

--- client/js/process-login.js
+++ client/js/process-login.js
@@ -3,53 +3,25 @@ function displayErrorInFields(errors, field){
     document.getElementById(field+'Error').innerHTML = '<h1 class="errorField">'+ errors[field] +'</h1>';
 }
 
-function processSignUp(){
-    const url = 'https://deploy-challenge3-to-heroku.herokuapp.com/api/v1/auth/signup';
+function processLogIn(){
+    const url = 'https://deploy-challenge3-to-heroku.herokuapp.com/api/v1/auth/login';
     
-    let firstName = document.forms["signUp"]["firstName"].value;
-    let lastName = document.forms["signUp"]["lastName"].value;
-    let email = document.forms["signUp"]["email"].value;
-    let password = document.forms["signUp"]["password"].value;
-    let confirm_password = document.forms["signUp"]["confirm_password"].value;
-    let photograph = document.forms["signUp"]["photograph"].value;
+    let email = document.forms["signIn"]["email"].value;
+    let password = document.forms["signIn"]["password"].value;
 
-    document.getElementById('firstNameError').innerHTML = '';
-    document.getElementById('lastNameError').innerHTML = '';
     document.getElementById('emailError').innerHTML = '';
     document.getElementById('passwordError').innerHTML = '';
-    document.getElementById('confirm_passwordError').innerHTML = '';
-    document.getElementById('photographError').innerHTML = '';
 
-    const body = {
-        firstName,
-        lastName,
-        email,
-        password,
-        confirm_password
-    }
-
-    const fields = [
-        'firstName',
-        'lastName',
-        'email',
-        'password',
-        'confirm_password'
-    ]
-
-    
+    const fields = [ 'email', 'password']
     const dataForFetch = { 
         method: 'POST', 
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json"
-        }
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" }
     }
 
     fetch(url, dataForFetch)
     .then((res) => res.json()) 
     .then((data) => {
-        console.log(body);
-        console.log(data.errors)
         if(data.errors){
             if(data.errors !== null && typeof data.errors === 'object'){
                 fields.forEach((field) =>{
@@ -62,9 +34,9 @@ function processSignUp(){
                  = '<h1 class="errorField">'+ data.errors +'</h1>';
             }
             
-        }else if(data.success){
-            document.getElementById("successMessage").innerHTML
-            = '<h1 class="successField">'+ data.success +'</h1>';
+        }else if(data.message){
+            window.location.href = 'all-entries.html';
+            console.log(data.message);
         }
       })
       .catch((error) => {
