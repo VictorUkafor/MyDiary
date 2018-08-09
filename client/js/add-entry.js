0824ca4aad53f63ac14aeef4cbdfe69000d070b8
Victor Ukafor <victorukafor@gmail.com>
2018-08-09 08:38:08 +0100

    feat(consume-add-entry-api): Add logic for connecting the add entry form to add entry api using the fetch-API
    - Add logic for consumming the add-entry-api in the add-entry form
    - using the fetch-api.
    - Refactor the add-entry.html to have an onsubmit event to call the
    - function that processes the form.
    [Finishes #159628878]

---
+++ client/js/add-entry.js
@@ -0,0 +1,42 @@
+
+function displayErrorInFields(errors, field){
+    document.getElementById(field+'Error').innerHTML = '<h1 class="errorField">'+ errors[field] +'</h1>';
+}
+
+function processAddEntry(){
+    const url = 'https://deploy-challenge3-to-heroku.herokuapp.com/api/v1/entries';
+    const token = localStorage.getItem('token');
+
+    let title = document.forms["addEntry"]["title"].value;
+    let content = document.forms["addEntry"]["content"].value;
+    
+    const dataForFetch = { 
+        method: 'POST', 
+        body: JSON.stringify({ title, content }),
+        headers: {
+            "Content-Type": "application/json",
+            "authentication": token
+        }
+    }
+
+    fetch(url, dataForFetch)
+    .then((res) => res.json()) 
+    .then((data) => {
+        console.log(data);
+        if(data.authenticated === false){
+            window.location.href = 'sign-in.html';
+            document.getElementById("errorMessage").innerHTML =
+            '<h1 class="errorField"> You have to login! </h1>'; 
+        } else if(data.errors) {
+            document.getElementById('errorMessage').innerHTML = 
+            '<h1 class="errorField">'+ data.errors +'</h1>';
+        } else {
+            window.location.href = 'all-entries.html';
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+      return false;
+}
