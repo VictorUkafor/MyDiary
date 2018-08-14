2187cd4c53d28b021e841bf79a235a01353e9e3f
Victor Ukafor <victorukafor@gmail.com>
2018-08-14 02:24:44 +0100

    bug(redesign-UI): Modify the profile page and error messages to improve user experience
    - The color scheme of the profile page do not match with the color
    - of the UI. Also the error messages displayed on form submission or
    - on redirection do not fit properly into the screen. This commit fixes
    - both bugs described above.
    [Fixes #159722037]

--- client/js/redirect-message.js
+++ client/js/redirect-message.js
@@ -3,12 +3,12 @@ const logout = localStorage.getItem('logout');
 
 if(login){
 document.getElementById('errorMessage2').innerHTML =
- '<h1 class="errorField">' + login + '</h1>';
+ '<h1 class="errorField2">' + login + '</h1>';
 }
 
 if(logout){
-    document.getElementById('successMessage').innerHTML =
-     '<h1 class="successField">' + logout + '</h1>';
+    document.getElementById('successMessage2').innerHTML =
+     '<h1 class="successField2">' + logout + '</h1>';
  }
 
 window.localStorage.removeItem('login');
